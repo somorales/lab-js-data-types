0f563b3ece632135f6a662040c51d7f3941c92f6
Sofia Morales <sofimoralesroa92@gmail.com>
2024-08-28 22:04:59 +0200

    cambios

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ const s2 = "fed";
 const s3 = "Ted";
 const s4 = "bread";
 const s5 = "and";
+const tongueTwister= (`${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`)
+
+
+    
+    
 
 // Concatenate the string variables into one new string
 
 
 // Print out the concatenated string
 
-
+console.log (tongueTwister)
 
 
 /*******************************************
@@ -20,25 +25,28 @@ const s5 = "and";
 *******************************************/
 const part1 = "java";
 const part2 = "script";
-
+let sumaP= `${part1.slice(0,part1.length-1)}${part1.toUpperCase()[part1.length-1]}${part2.slice(0,part2.length-1)}${part2.toUpperCase()[part2.length-1]}`
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+
 
 
 // Print the cameLtaiL-formatted string
 
-
+console.log(sumaP)
 
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
 *******************************************/
 const billTotal = 84;
+tipAmount= 84 * 0.15
+
 
 // Calculate the tip (15% of the bill total)
 
 
 // Print out the tipAmount
-
+console.log (tipAmount)
 
 
 
@@ -49,9 +57,10 @@ const billTotal = 84;
 // Generate a random integer between 1 and 10 (inclusive)
 
 
+
 // Print the generated random number
 
-
+console.log(Math.random()*10)
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -61,16 +70,16 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; //Flaso
 
-const expression2 = a || b;
+const expression2 = a || b; //verdadero
 
-const expression3 = !a && b;
+const expression3 = !a && b; //falso
 
-const expression4 = !(a && b);
+const expression4 = !(a && b); //verdadero
 
-const expression5 = !a || !b;
+const expression5 = !a || !b; //verdadero 
 
-const expression6 = !(a || b);
+const expression6 = !(a || b);//Falso
 
-const expression7 = a && a;
+const expression7 = a && a;//verdadero
